2eb922091da6ff2ab235f80675e4a01c2be14cf8
tungnhs <tungnhs2014@gmail.com>
2024-08-11 21:37:59 +0700

    Add exercise Bai_14

---
+++ Bai_14/Bai_13.py
@@ -0,0 +1,31 @@
+'''
+Cho trước N ngày, hãy đổi N thành số năm, số tuần và số ngày. Biết rằng một năm có 365 ngày.
+
+Input Format
+Số nguyên không âm N
+
+Constraints
+0<=N<=1000000
+
+Output Format
+In ra số năm, tuần, ngày tương ứng với N ngày
+
+Sample Input 0
+373
+
+Sample Output 0
+1 1 1
+'''
+
+n = int(input())
+
+nam = n // 365
+
+n %= 365
+
+tuan = n // 7
+
+ngay = n % 7
+
+print(nam, tuan , ngay)
+
---
+++ Bai_14/Bai_15.py
@@ -0,0 +1,35 @@
+'''
+28tech muốn nấu một món súp. Để làm điều đó, anh ta cần mua chính xác n lít nước. 
+Chỉ có hai loại chai nước trong cửa hàng gần đó - chai 1 lít và chai 2 lít. Có vô số chai của hai loại này trong cửa hàng. 
+Chai loại thứ nhất có gía a burles và chai loại thứ hai có giá tương ứng b burles. 28tech muốn chi càng ít tiền càng tốt.
+Nhiệm vụ của bạn là tìm ra số tiền tối thiểu (bằng burles) 28tech cần mua chính xác n lít nước ở cửa hàng gần đó nếu chai loại thứ nhất 
+có giá a burles và chai loại thứ hai có giá b burles.
+
+Gợi ý : Kiểm tra xem chai nào rẻ hơn (giá tiền 1Lit) thì sẽ ưu tiên mua chai đó, nếu chai 1l rẻ hơn thì mua toàn bộ n chai 1l, 
+còn chai 2lit rẻ hơn thì chia làm 2 trường hợp. N chẵn thì mua n / 2 chai 2l, n lẻ thì phải mua thêm 1 chai 1l vì không được mua thừa.
+
+Input Format
+3 số n,a,b là số nguyên
+
+Constraints
+1<=n<=10^12; 1<=a,b<=1000
+
+Output Format
+Số tiền ít nhất để mua được n lit nước. Chú ý bạn phải mua chính xác n lít nước, không mua thiếu cũng không mua thừa.
+
+Sample Input 0
+10 1 3
+
+Sample Output 0
+10
+'''
+
+n, a, b = map(int, input().split())
+
+if a <= 2*b:
+    print(n*a)
+else:
+    if n % 2 == 0:
+        print(n // 2 * b)
+    else:
+        print((n-1)// 2 * b  + a)
